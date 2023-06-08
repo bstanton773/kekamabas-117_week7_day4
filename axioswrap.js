446f19688455fcb59f9abcebdbf1a15f35a26846
Brian <bstanton773@gmail.com>
2023-06-08 15:35:03 -0500

    Add login and apiClientBasicAuth

--- axioswrap.js
+++ axioswrap.js
@@ -7,6 +7,13 @@ const questionEndpoint = '/question';
 
 const apiClientNoAuth = () => axios.create({
     baseURL: base
+});
+
+const apiClientBasicAuth = (email, password) => axios.create({
+    baseURL: base,
+    headers: {
+        Authorization: 'Basic ' + btoa(email + ':' + password)
+    }
 })
 
 
@@ -19,6 +26,28 @@ const registerUser = async (data) => {
     }
     return {
         error
+    }
+}
+
+
+const loginUser = async (email, password) => {
+    let error;
+    let user;
+
+    try{
+        const response = await apiClientBasicAuth(email, password).get(loginEndpoint);
+        user = response.data
+    } catch (err) {
+        if (err.response.status===401){
+            error = "Invalid Email/Password Combo"
+        } else {
+            error = 'An unexpected error occurred. Please try again later.'
+        }
+    }
+
+    return {
+        error,
+        user
     }
 }
 
@@ -60,3 +89,18 @@ const reg = async () => {
 
 // Call function to create the user (only do it once or there will be errors)
 // reg();
+
+
+// test logging a user in  
+const login = async () => {
+    let result = await loginUser('mjordan23@bulls.com', '6rings');
+    console.log(result.error ?? 'Success Connecting');
+    console.log(result.user ?? 'Bad Login')
+    console.log('your token is ', result.user.token);
+}
+
+login();
+
+
+// Save the token for our future token auth endpoints
+const myToken = 'mwftJkqXxz2cqrK2-blrcN8Xtk-4ETqzN5A84Cmz6Zk'
